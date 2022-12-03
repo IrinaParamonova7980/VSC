852a7aa95479d89db3e404024a1c4109d9b69881
IrinaParamonova7980 <irisha-7980@yandex.ru>
2022-12-03 07:57:01 +0300

    study

---
+++ JavaScript/Занятие1/calculator.js
@@ -0,0 +1,31 @@
+function calcSum() {
+    let a = prompt("Введите первое число","");
+    let b = prompt("Введите второе число","");
+        alert(Number(a)+Number(b));
+  }
+  
+  calcSum()
+
+  function calcSubtraction() {
+    let a = prompt("Введите первое число","");
+    let b = prompt("Введите второе число","");
+        alert(Number(a)-Number(b));
+  }
+  
+  calcSubtraction()
+
+  function calcDivision() {
+    let a = prompt("Введите первое число","");
+    let b = prompt("Введите второе число","");
+        alert(Number(a)/Number(b));
+  }
+  
+  calcDivision()
+
+  function calcMultiplication() {
+    let a = prompt("Введите первое число","");
+    let b = prompt("Введите второе число","");
+        alert(Number(a)*Number(b));
+  }
+  
+  calcMultiplication()
