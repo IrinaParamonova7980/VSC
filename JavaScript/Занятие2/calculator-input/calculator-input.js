17a54ea115bc1624e2b2f04a8f842057105a5758
IrinaParamonova7980 <irisha-7980@yandex.ru>
2022-12-15 06:29:55 +0300

    На ноль делить - вывод на экран

--- JavaScript/Занятие2/calculator-input/calculator-input.js
+++ JavaScript/Занятие2/calculator-input/calculator-input.js
@@ -1,14 +1,14 @@
 function enterNumberAndCount(operation) {
   const [a, b] = enterNumber();
   const result = outputTheResult(a, b, operation);
-  document.getElementById('total').value = result;
+  document.getElementById("total").value = result;
 }
 
 function enterNumber() {
-  let a = Number(document.getElementById('first-number').value);
-  let b = Number(document.getElementById('second-number').value);
-   return [a, b]; 
-  }
+  let a = Number(document.getElementById("first-number").value);
+  let b = Number(document.getElementById("second-number").value);
+  return [a, b];
+}
 
 function outputTheResult(a, b, operation) {
   switch (operation) {
@@ -20,17 +20,15 @@ function outputTheResult(a, b, operation) {
       break;
     case "/":
       if (b == 0) {
-        alert("На ноль делить нельзя");
+        document.getElementById("divisionByZero").innerHTML +=
+          "На ноль делить нельзя!";
+      } else {
+        res = a / b;
       }
-      else { res = a / b };
-            break;
+      break;
     case "*":
       res = a + b;
       break;
   }
   return res;
-  }
-
-
-
- 
+}
