d27115d61beb9b5f1ddb4508c2268a80eb6558d5
IrinaParamonova7980 <irisha-7980@yandex.ru>
2022-12-15 05:56:05 +0300

    Выбор цвета темы

---
+++ JavaScript/Занятие4/theme/theme.js
@@ -0,0 +1,8 @@
+function selectionBackground() {
+  if (document.getElementById("backgroundcolor").value == "white")
+    document.body.style.background = "white";
+  if (document.getElementById("backgroundcolor").value == "blue")
+    document.body.style.background = "blue";
+  if (document.getElementById("backgroundcolor").value == "green")
+    document.body.style.background = "green";
+}
